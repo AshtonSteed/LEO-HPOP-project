5bdbaac41db9aeed2de2c74846595107e4e06944
Maybelline Grace <69809000+MayTheCutie@users.noreply.github.com>
2025-02-14 10:59:40 -0500

    Preparing for Wednesday's meeting

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ import pandas as pd
 
 print("Hello World 2!")
 
+#hi there, this is may
+
+#U = -(G*M_earth)/r) + {a}
+#    sum(N_z)(n=2)[(J_n*P_n^0*sin(theta))/(r^(n+1)] + {b}
+#    sum(N_t)(n=2)[sum(n)(m=1)[(P_n^m*sin(theta)*(C_n^m*cos(m*phi)+S_n^m*sin(m*phi)))/(r^(n+1))] {c,d}
+
+#
+def
