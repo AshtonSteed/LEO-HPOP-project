c8c0e3ab34f088803833444bd6b4a02fbbc62f13
Maybelline Grace <69809000+MayTheCutie@users.noreply.github.com>
2025-04-09 17:38:51 -0400

    added some models from chatgpt

--- state.py
+++ state.py
@@ -2,10 +2,12 @@ import numpy as np
 import pandas as pd
 import scipy as sp
 import butchertableau as bt
+import gravity as g
 
 # U = -(G*M_earth)/r) + {a}
 #    sum(N_z)(n=2)[(J_n*P_n^0*sin(theta))/(r^(n+1)] + {b}
-#    sum(N_t)(n=2)[sum(n)(m=1)[(P_n^m*sin(theta)*(C_n^m*cos(m*phi)+S_n^m*sin(m*phi)))/(r^(n+1))] {c,d}
+#    sum(N_t)(n=2)[sum(n)(m=1)[(P_n^m*sin(theta)* {c}
+#                              (C_n^m*cos(m*phi)+S_n^m*sin(m*phi)))/(r^(n+1))]] {d}
 
 # Before you begin typing ANY code, you MUST write a comment of what you are intending to do with the function.
 # An example would be:
